f0593986b2ec8d1539b3f87388d2acea9a2f83b4
2code <2mail2code@gmail.com>
2024-04-19 12:04:01 +0200

    update and add unittests for csvUpdaterService

---
+++ src/Code2.Data.GeoIPTests/CsvUpdateServiceTests.cs
@@ -0,0 +1,82 @@
+﻿using Code2.Data.GeoIP;
+using Code2.Data.GeoIP.Internals;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
+using NSubstitute.ExceptionExtensions;
+
+namespace Code2.Data.GeoIP.Tests
+{
+	[TestClass]
+	public class CsvUpdateServiceTests
+	{
+		private IFileSystem _fileSystem = default!;
+		private IHttpUtility _httpUtility = default!;
+		private ITaskUtility _taskUtility = default!;
+		private const int _msPerHour = 3600000;
+
+		[TestMethod]
+		public void UpdateFilesAsync_When_FileUpdate_Expect_CorrectIsUpdatingValue()
+		{
+			ResetDependencies();
+			_fileSystem.FileExists(Arg.Any<string>()).Returns(false);
+			_httpUtility.DownloadFileToAsync(Arg.Any<string>(), Arg.Any<string>(), default).ReturnsForAnyArgs(x => Task.Delay(10));
+			CsvUpdateService csvUpdateService = new CsvUpdateService(_fileSystem, _httpUtility, _taskUtility);
+
+			csvUpdateService.UpdateFilesAsync().Wait(0);
+			bool shouldBeTrue = csvUpdateService.IsUpdating;
+			Thread.Sleep(50);
+			bool shouldBeFalse = csvUpdateService.IsUpdating;
+
+			Assert.IsTrue(shouldBeTrue);
+			Assert.IsFalse(shouldBeFalse);
+		}
+
+		[TestMethod()]
+		[DataRow(-75, -76, 6)]	//local file older than 3 days but no newer remote file, expect 6 hour delay
+		[DataRow(-24, -76, 24)]	//day old local file but no newer remote file, expect 24 hour delay
+		[DataRow(-5, -2, 72)]	//newer remote file, runs update and delays 72 hours
+		public void StartAutomaticUpdating_When_FileLastModifiedComparedToRemote_Expect_Delay(int addHoursFile, int addHoursRemote, int expectedHoursDelay)
+		{
+			Console.WriteLine("addHoursFile: {0}, addHoursRemote: {1}, expectedHoursDelay: {2}", addHoursFile, addHoursRemote, expectedHoursDelay);
+			ResetDependencies();
+			DateTime lastModifiedLocal = DateTime.Now.AddHours(addHoursFile);
+			DateTime lastModifiedRemote = DateTime.Now.AddHours(addHoursRemote);
+			_fileSystem.FileExists(Arg.Any<string>()).Returns(false);
+			_fileSystem.DirectoryGetFiles(Arg.Any<string>(), Arg.Any<string>()).Returns(new[] { "blockipv4.csv" });
+			_fileSystem.FileGetLastWriteTime(Arg.Any<string>()).Returns(lastModifiedLocal);
+			_httpUtility.GetLastModifiedHeaderAsync(Arg.Any<string>()).Returns(lastModifiedRemote);
+			_taskUtility.Delay(Arg.Any<int>(), Arg.Any<CancellationToken>()).Returns(x => Task.Delay(10, (CancellationToken)x[1]));
+			CsvUpdateService csvUpdateService = new CsvUpdateService(_fileSystem, _httpUtility, _taskUtility);
+			csvUpdateService.CsvFileFilters = new[] { "blockipv4" };
+			
+			csvUpdateService.StartAutomaticUpdating();
+			Thread.Sleep(5);
+			csvUpdateService.StopAutomaticUpdating();
+
+			_taskUtility.Received(1).Delay(_msPerHour * expectedHoursDelay, Arg.Any<CancellationToken>());
+		}
+
+		[TestMethod()]
+		public void StartAutomaticUpdating_When_ExceptionOccurs_Expect_Delay1Hour()
+		{
+			ResetDependencies();
+			_httpUtility.GetLastModifiedHeaderAsync(Arg.Any<string>()).Throws(new InvalidOperationException());
+			_taskUtility.Delay(Arg.Any<int>(), Arg.Any<CancellationToken>()).Returns(x => Task.Delay(10, (CancellationToken)x[1]));
+			CsvUpdateService csvUpdateService = new CsvUpdateService(_fileSystem, _httpUtility, _taskUtility);
+			csvUpdateService.Error += (_, _) => { };
+
+			csvUpdateService.StartAutomaticUpdating();
+			Thread.Sleep(5);
+			csvUpdateService.StopAutomaticUpdating();
+
+			_taskUtility.Received(1).Delay(_msPerHour, Arg.Any<CancellationToken>());
+		}
+
+		private void ResetDependencies()
+		{
+			_fileSystem = Substitute.For<IFileSystem>();
+			_httpUtility = Substitute.For<IHttpUtility>();
+			_taskUtility = Substitute.For<ITaskUtility>();
+		}
+	}
+}
---
+++ src/Code2.Data.GeoIPTests/HttpUtilityTests.cs
@@ -0,0 +1,131 @@
+﻿using Code2.Data.GeoIP.Internals;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
+using System.Collections;
+using System.Collections.Specialized;
+using System.Net.Http.Headers;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Code2.Data.GeoIP.Tests
+{
+	[TestClass]
+	public class HttpUtilityTests
+	{
+		private HttpRequestMessage _requestMessage = default!;
+		private HttpRequestMessage _defaultRequestMessage = default!;
+		private HttpResponseMessage _responseMessage = default!;
+		private IHttpClient _httpClient = default!;
+		private HttpRequestHeaders _defaultRequestHeaders = default!;
+		private IHttpClientFactory _httpClientFactory = default!;
+		private IFileSystem _fileSystem = default!;
+
+		[TestMethod]
+		[ExpectedException(typeof(AggregateException))]
+		public void GetLastModifiedHeaderAsync_When_ResponseNotIsSuccess_Expect_Exception()
+		{
+			ResetDependencies();
+			_responseMessage.StatusCode = System.Net.HttpStatusCode.BadRequest;
+			_httpClient.SendAsync(Arg.Any<HttpRequestMessage>()).Returns(_responseMessage);
+			_httpClientFactory.Create().Returns(_httpClient);
+
+			HttpUtility httpUtility = new HttpUtility(_httpClientFactory, _fileSystem);
+			httpUtility.GetLastModifiedHeaderAsync("http://localhost/").Wait();
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(AggregateException))]
+		public void GetLastModifiedHeaderAsync_When_LastModifiedHeaderAbsent_Expect_Exception()
+		{
+			ResetDependencies();
+			_httpClient.SendAsync(Arg.Any<HttpRequestMessage>()).Returns(_responseMessage);
+			_httpClientFactory.Create().Returns(_httpClient);
+
+			HttpUtility httpUtility = new HttpUtility(_httpClientFactory, _fileSystem);
+			httpUtility.GetLastModifiedHeaderAsync("http://localhost/").Wait();
+		}
+
+		[TestMethod]
+		public void GetLastModifiedHeaderAsync_When_HeaderSetCorrect_Expect_CorrectValue()
+		{
+			ResetDependencies();
+			DateTime dateTime = new DateTime(2000, 1, 1, 0, 0, 0);
+			_responseMessage.Content.Headers.Add("last-modified", dateTime.ToString("r"));
+			_httpClient.SendAsync(Arg.Any<HttpRequestMessage>()).Returns(_responseMessage);
+			_httpClientFactory.Create().Returns(_httpClient);
+
+			HttpUtility httpUtility = new HttpUtility(_httpClientFactory, _fileSystem);
+			DateTime result = httpUtility.GetLastModifiedHeaderAsync("http://localhost/").Result;
+
+			Assert.AreEqual(dateTime.ToLocalTime(), result);
+		}
+
+		[TestMethod]
+		public void DownloadFileToAsync_When_ResponseStream_Expect_FileStreamWritten()
+		{
+			ResetDependencies();
+			byte[] bufferIn = Encoding.UTF8.GetBytes("0123456789abcdef");
+			using MemoryStream msIn = new MemoryStream(bufferIn);
+			using MemoryStream msOut = new MemoryStream();
+			_httpClient.GetStreamAsync(Arg.Any<string>()).Returns(msIn);
+			_fileSystem.FileCreate(Arg.Any<string>()).Returns(msOut);
+			_httpClientFactory.Create().Returns(_httpClient);
+			HttpUtility httpUtility = new HttpUtility(_httpClientFactory, _fileSystem);
+			
+			httpUtility.DownloadFileToAsync("http://localhost/", "/path/to/file").Wait();
+
+			byte[] bufferOut = msOut.ToArray();
+			int compareResult = StructuralComparisons.StructuralComparer.Compare(bufferIn, bufferOut);
+
+			Assert.AreEqual(0, compareResult);
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(AggregateException))]
+		public void DownloadFileToAsync_When_HashMismatch_Expect_Exception()
+		{
+			ResetDependencies();
+			string hash1 = "abcdef";
+			string hash2 = "012345";
+			using MemoryStream msIn = new MemoryStream();
+			using MemoryStream msOut = new MemoryStream();
+			_fileSystem.FileCreate(Arg.Any<string>()).Returns(msOut);
+			_fileSystem.FileGetSha256Hex(Arg.Any<Stream>()).Returns(hash2);
+			_httpClient.GetStreamAsync(Arg.Any<string>()).Returns(msIn);
+			_httpClient.GetStringAsync(Arg.Any<string>()).Returns(hash1);
+			_httpClientFactory.Create().Returns(_httpClient);
+			HttpUtility httpUtility = new HttpUtility(_httpClientFactory, _fileSystem);
+
+			httpUtility.DownloadFileToAsync("http://localhost/", "/path/to/file", hash1).Wait();
+		}
+
+		[TestMethod]
+		public void DownloadFileToAsync_When_HashMatch_Expect_NoProblem()
+		{
+			ResetDependencies();
+			string hash1 = "abcdef";
+			using MemoryStream msIn = new MemoryStream();
+			using MemoryStream msOut = new MemoryStream();
+			_fileSystem.FileCreate(Arg.Any<string>()).Returns(msOut);
+			_fileSystem.FileGetSha256Hex(Arg.Any<Stream>()).Returns(hash1);
+			_httpClient.GetStreamAsync(Arg.Any<string>()).Returns(msIn);
+			_httpClient.GetStringAsync(Arg.Any<string>()).Returns(hash1);
+			_httpClientFactory.Create().Returns(_httpClient);
+			HttpUtility httpUtility = new HttpUtility(_httpClientFactory, _fileSystem);
+
+			httpUtility.DownloadFileToAsync("http://localhost/", "/path/to/file", hash1).Wait();
+		}
+
+		private void ResetDependencies()
+		{
+			_requestMessage = new HttpRequestMessage();
+			_responseMessage = new HttpResponseMessage();
+			_defaultRequestMessage = new HttpRequestMessage();
+			_httpClient = Substitute.For<IHttpClient>();
+			_httpClient.DefaultRequestHeaders.Returns(_defaultRequestMessage.Headers);
+			_httpClientFactory = Substitute.For<IHttpClientFactory>();
+			_fileSystem = Substitute.For<IFileSystem>();
+			
+		}
+	}
+}

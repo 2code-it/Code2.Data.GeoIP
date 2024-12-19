b565ba466f26e42a0f6e0c38f8d15e2ca3b8d857
2code <2mail2code@gmail.com>
2024-12-19 22:23:59 +0100

    remove unused fucntions from filesystem

--- src/Code2.Data.GeoIP/Internals/FileSystem.cs
+++ src/Code2.Data.GeoIP/Internals/FileSystem.cs
@@ -1,73 +1,44 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
 using System.IO.Compression;
-using System.Linq;
 using System.Security.Cryptography;
 
-namespace Code2.Data.GeoIP.Internals
+namespace Code2.Data.GeoIP.Internals;
+
+internal class FileSystem : IFileSystem
 {
-	internal class FileSystem : IFileSystem
+	public string PathGetFullPath(string path)
+		=> Path.GetFullPath(path);
+
+	public string PathCombine(params string[] paths)
+		=> Path.Combine(paths);
+
+	public bool FileExists(string path)
+		=> File.Exists(path);
+
+	public Stream FileOpenRead(string path)
+		=> File.OpenRead(path);
+
+	public Stream? GetManifestResourceStream(Type type, string name)
+		=> type.Assembly.GetManifestResourceStream(type, name);
+
+	public string FileGetSha256Hex(string filePath)
 	{
-		public string PathGetFullPath(string path)
-			=> Path.GetFullPath(path, AppDomain.CurrentDomain.BaseDirectory);
+		using var fileStream = File.OpenRead(filePath);
+		byte[] hashBytes = SHA256.HashData(fileStream);
+		fileStream.Close();
+		return Convert.ToHexString(hashBytes);
+	}
 
-		public StreamReader FileOpenText(string path)
-			=> File.OpenText(path);
+	public void ZipArchiveExtracTo(string zipFilePath, string outputDirectory)
+	{
+		using Stream zipFileStream = File.OpenRead(zipFilePath);
+		using ZipArchive zipArchive = new ZipArchive(zipFileStream);
 
-		public Stream FileOpenRead(string path)
-			=> File.OpenRead(path);
-
-		public Stream FileOpen(string path)
-			=> File.Open(path, FileMode.Open, FileAccess.ReadWrite);
-
-		public Stream FileCreate(string path)
-			=> File.Create(path);
-
-		public void FileDelete(string path)
-			=> File.Delete(path);
-
-		public bool FileExists(string path)
-			=> File.Exists(path);
-
-		public DateTime FileGetLastWriteTime(string path)
-			=> File.GetLastWriteTime(path);
-
-		public string FileReadAllText(string path)
-			=> File.ReadAllText(path);
-
-		public void FileAppendAllLines(string path, IEnumerable<string> contents)
-			=> File.AppendAllLines(path, contents);
-
-		public string PathCombine(params string[] paths)
-			=> Path.Combine(paths);
-
-		public string[] DirectoryGetFiles(string path, string search)
-			=> Directory.GetFiles(path, search);
-
-		public void DirectoryCreate(string path)
-			=> Directory.CreateDirectory(path);
-
-		public bool DirectoryExists(string path)
-			=> Directory.Exists(path);
-
-		public string FileGetSha256Hex(Stream stream)
+		foreach (var entry in zipArchive.Entries)
 		{
-			byte[] hashBytes = SHA256.HashData(stream);
-			return Convert.ToHexString(hashBytes);
-		}
-
-		public void ZipArchiveExtractEntryTo(string zipFilePath, string fileFilter, string outputDirectory)
-		{
-			using Stream zipFileStream = FileOpenRead(zipFilePath);
-			using ZipArchive zipArchive = new ZipArchive(zipFileStream);
-
-			var entry = zipArchive.Entries.FirstOrDefault(x => x.Name.Contains(fileFilter));
-			if (entry is null) return;
-
-			string filePath = PathCombine(outputDirectory, entry.Name);
+			string filePath = Path.Combine(outputDirectory, entry.Name);
 			entry.ExtractToFile(filePath, true);
 		}
-
 	}
 }
--- src/Code2.Data.GeoIP/Internals/IFileSystem.cs
+++ src/Code2.Data.GeoIP/Internals/IFileSystem.cs
@@ -1,26 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
 
-namespace Code2.Data.GeoIP.Internals
+namespace Code2.Data.GeoIP.Internals;
+internal interface IFileSystem
 {
-	internal interface IFileSystem
-	{
-		StreamReader FileOpenText(string path);
-		Stream FileOpenRead(string path);
-		Stream FileOpen(string path);
-		Stream FileCreate(string path);
-		void FileDelete(string path);
-		bool FileExists(string path);
-		string FileReadAllText(string path);
-		DateTime FileGetLastWriteTime(string path);
-		void FileAppendAllLines(string path, IEnumerable<string> contents);
-		string PathGetFullPath(string path);
-		string PathCombine(params string[] paths);
-		string[] DirectoryGetFiles(string path, string search);
-		void DirectoryCreate(string path);
-		bool DirectoryExists(string path);
-		string FileGetSha256Hex(Stream stream);
-		void ZipArchiveExtractEntryTo(string zipFilePath, string fileFilter, string outputDirectory);
-	}
+	string PathGetFullPath(string path);
+	string PathCombine(params string[] paths);
+	bool FileExists(string path);
+	Stream FileOpenRead(string path);
+	Stream? GetManifestResourceStream(Type type, string name);
+	string FileGetSha256Hex(string filePath);
+	void ZipArchiveExtracTo(string zipFilePath, string outputDirectory);
 }

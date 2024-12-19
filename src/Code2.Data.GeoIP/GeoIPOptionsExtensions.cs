89e6ada41487115ef83da50953a9d124f56c4fc1
2code <2mail2code@gmail.com>
2024-12-19 22:28:14 +0100

    add geoIpOptions extensions

---
+++ src/Code2.Data.GeoIP/GeoIPOptionsExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code2.Data.GeoIP;
+public static class GeoIPOptionsExtensions
+{
+	public static GeoIPOptions UseTypes<Tblock, Tlocation>(this GeoIPOptions options)
+		=> options.UseBlockType<Tblock>().UseLocationType<Tlocation>();
+
+	public static GeoIPOptions UseTypes<Tblock, Tlocation, Tisp>(this GeoIPOptions options)
+		=> options.UseBlockType<Tblock>().UseLocationType<Tlocation>().UseIspType<Tisp>();
+	
+	public static GeoIPOptions UseBlockType<T>(this GeoIPOptions options)
+	{
+		options.BlockTypeName = typeof(T).FullName;
+		return options;
+	}
+
+	public static GeoIPOptions UseLocationType<T>(this GeoIPOptions options)
+	{
+		options.LocationTypeName = typeof(T).FullName;
+		return options;
+	}
+
+	public static GeoIPOptions UseIspType<T>(this GeoIPOptions options)
+	{
+		options.IspTypeName = typeof(T).FullName;
+		return options;
+	}
+}
---
+++ src/Code2.Data.GeoIPTests/GeoIPOptionsExtensionsTests.cs
@@ -0,0 +1,51 @@
+﻿using Code2.Data.GeoIP;
+using Code2.Data.GeoIPTests.Assets;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Code2.Data.GeoIPTests;
+
+[TestClass]
+public class GeoIPOptionsExtensionsTests
+{
+	[TestMethod]
+	public void UseBlockType_When_Used_Expect_BlockTypeNameSet()
+	{
+		GeoIPOptions options = new();
+
+		options.UseBlockType<TestItem>();
+
+		Assert.AreEqual(typeof(TestItem).FullName, options.BlockTypeName);
+	}
+
+	[TestMethod]
+	public void UseLocationType_When_Used_Expect_LocationTypeNameSet()
+	{
+		GeoIPOptions options = new();
+
+		options.UseLocationType<TestItem>();
+
+		Assert.AreEqual(typeof(TestItem).FullName, options.LocationTypeName);
+	}
+
+	[TestMethod]
+	public void UseIspType_When_Used_Expect_IspTypeNameSet()
+	{
+		GeoIPOptions options = new();
+
+		options.UseIspType<TestItem>();
+
+		Assert.AreEqual(typeof(TestItem).FullName, options.IspTypeName);
+	}
+
+	[TestMethod]
+	public void UseTypes_When_Used_Expect_TypeNamesSet()
+	{
+		GeoIPOptions options = new();
+
+		options.UseTypes<TestItem, TestItem, TestItem>();
+
+		Assert.AreEqual(typeof(TestItem).FullName, options.BlockTypeName);
+		Assert.AreEqual(typeof(TestItem).FullName, options.LocationTypeName);
+		Assert.AreEqual(typeof(TestItem).FullName, options.IspTypeName);
+	}
+}

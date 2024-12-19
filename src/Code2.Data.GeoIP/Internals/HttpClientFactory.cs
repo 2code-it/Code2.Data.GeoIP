1f05e94809e6f225a73ddc023c2f97375e17f72d
2code <2mail2code@gmail.com>
2024-12-19 22:24:58 +0100

    remove unused classes

--- src/Code2.Data.GeoIP/Internals/HttpClientFactory.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Code2.Data.GeoIP.Internals
-{
-	internal class HttpClientFactory : IHttpClientFactory
-	{
-		public IHttpClient Create()
-			=> new HttpClient(new System.Net.Http.HttpClient());
-	}
-}
--- src/Code2.Data.GeoIP/Internals/IHttpClientFactory.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Code2.Data.GeoIP.Internals
-{
-	internal interface IHttpClientFactory
-	{
-		IHttpClient Create();
-	}
-}

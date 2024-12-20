be50348bd3ddea92146272d067bd1ecdd8172c41
2code <2mail2code@gmail.com>
2024-12-20 19:21:53 +0100

    add useTypes extensions to geoIPOptions

--- src/Code2.Data.GeoIP/GeoIPOptionsExtensions.cs
+++ src/Code2.Data.GeoIP/GeoIPOptionsExtensions.cs
@@ -1,31 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Code2.Data.GeoIP.Models;
 
 namespace Code2.Data.GeoIP;
 public static class GeoIPOptionsExtensions
 {
 	public static GeoIPOptions UseTypes<Tblock, Tlocation>(this GeoIPOptions options)
+		where Tblock : BlockBase
+		where Tlocation : LocationBase
 		=> options.UseBlockType<Tblock>().UseLocationType<Tlocation>();
 
 	public static GeoIPOptions UseTypes<Tblock, Tlocation, Tisp>(this GeoIPOptions options)
+		where Tblock : BlockBase
+		where Tlocation : LocationBase
+		where Tisp : IspBase
 		=> options.UseBlockType<Tblock>().UseLocationType<Tlocation>().UseIspType<Tisp>();
-	
+
 	public static GeoIPOptions UseBlockType<T>(this GeoIPOptions options)
+		where T : BlockBase
 	{
 		options.BlockTypeName = typeof(T).FullName;
 		return options;
 	}
 
 	public static GeoIPOptions UseLocationType<T>(this GeoIPOptions options)
+		where T : LocationBase
 	{
 		options.LocationTypeName = typeof(T).FullName;
 		return options;
 	}
 
 	public static GeoIPOptions UseIspType<T>(this GeoIPOptions options)
+		where T : IspBase
 	{
 		options.IspTypeName = typeof(T).FullName;
 		return options;

eb3c0833b848ea697d7b9e5163c8e54d96540503
2code <2mail2code@gmail.com>
2024-12-21 05:25:12 +0100

    add testitems according to type constraints in geoIPOptions useTypes methods

---
+++ src/Code2.Data.GeoIPTests/Assets/TestLocation.cs
@@ -0,0 +1,11 @@
+﻿using Code2.Data.GeoIP.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code2.Data.GeoIPTests.Assets;
+public class TestLocation: LocationBase
+{
+}

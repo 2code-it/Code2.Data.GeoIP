8a8ab221d4f6823f6210b73f8aa94e203a0a5600
2code <2mail2code@gmail.com>
2025-01-11 22:29:33 +0100

    add retryIntervalInHours to options

--- src/Code2.Data.GeoIP/MaxmindMetaOptions.cs
+++ src/Code2.Data.GeoIP/MaxmindMetaOptions.cs
@@ -4,6 +4,7 @@ namespace Code2.Data.GeoIP;
 public class MaxmindMetaOptions
 {
 	public int UpdateIntervalInHours { get; set; }
+	public int RetryIntervalInHours { get; set; }
 	public string DownloadUrl { get; set; } = string.Empty;
 	public string[] SupportedLanguages { get; set; } = Array.Empty<string>();
 	public string DefaultLanguage { get; set; } = string.Empty;
